1c77740e7a6c505b1328cc89d850b94342d79f11
NikitaPoruchikov <neoboom34@gmail.com>
2024-10-26 10:56:15 +0700

    fix bug

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -5,14 +5,14 @@ from .constants import PAGE_SIZE, MAX_PAGE_SIZE
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = PAGE_SIZE  # Количество объектов на одной странице по умолчанию
-    page_size_query_param = 'limit'  # Название параметра для изменения размера страницы
-    max_page_size = MAX_PAGE_SIZE  # Максимальный размер страницы
+    page_size = PAGE_SIZE
+    page_size_query_param = 'limit'
+    max_page_size = MAX_PAGE_SIZE
 
     def get_paginated_response(self, data):
         return Response({
-            'count': self.page.paginator.count,  # Общее количество объектов
-            'next': self.get_next_link(),  # Ссылка на следующую страницу
-            'previous': self.get_previous_link(),  # Ссылка на предыдущую страницу
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
             'results': data  # Сами данные
         })
