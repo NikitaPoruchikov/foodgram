a3b1edc84a32254d46d4ae2d90aab64af2dc542a
NikitaPoruchikov <neoboom34@gmail.com>
2024-10-27 17:43:05 +0700

    fix admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,41 +1,56 @@
 from django.contrib import admin
 
-from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                     Subscription, Tag)
+from .models import (Favorite, Ingredient,
+                     Recipe, RecipeIngredient, Subscription, Tag)
+
+
+class BaseAdminSettings(admin.ModelAdmin):
+    """Общие настройки для админ-панели."""
+    empty_value_display = '-пусто-'
 
 
 @admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(BaseAdminSettings):
     list_display = ('name', 'slug')
     search_fields = ('name',)
     prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Ingredient)
-class IngredientAdmin(admin.ModelAdmin):
+class IngredientAdmin(BaseAdminSettings):
     list_display = ('name', 'measurement_unit')
     search_fields = ('name',)
+    list_filter = ('name',)
 
 
 @admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'cooking_time')
+class RecipeAdmin(BaseAdminSettings):
+    list_display = ('name', 'author', 'cooking_time', 'get_favorite_count')
     search_fields = ('name', 'author__username')
-    list_filter = ('tags',)
+    list_filter = ('tags', 'author')
+    filter_horizontal = ('tags',)
+
+    def get_favorite_count(self, obj):
+        return obj.favorite_set.count()
+    get_favorite_count.short_description = 'Количество добавлений в избранное'
+    readonly_fields = ('get_favorite_count',)
 
 
 @admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):
+class SubscriptionAdmin(BaseAdminSettings):
     list_display = ('user', 'author', 'created_at')
     search_fields = ('user__username', 'author__username')
+    list_filter = ('author', 'user')
 
 
 @admin.register(Favorite)
-class FavoriteAdmin(admin.ModelAdmin):
+class FavoriteAdmin(BaseAdminSettings):
     list_display = ('user', 'recipe', 'added_at')
     search_fields = ('user__username', 'recipe__name')
+    list_filter = ('user',)
 
 
 @admin.register(RecipeIngredient)
-class RecipeIngredientAdmin(admin.ModelAdmin):
+class RecipeIngredientAdmin(BaseAdminSettings):
     list_display = ('recipe', 'ingredient', 'amount')
+    list_filter = ('ingredient',)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,14 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Subscribe
 
 
-class CustomUserAdmin(UserAdmin):
+class BaseAdminSettings(admin.ModelAdmin):
+    """Базовая кастомизация админ панели."""
+    empty_value_display = '-пусто-'
+
+
+class CustomUserAdmin(BaseAdminSettings, UserAdmin):
+    """Кастомизация админ панели для управления пользователями."""
     model = CustomUser
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('avatar',)}),
+        (None, {'fields': ('avatar', 'role')}),
     )
+    list_display = ('id', 'role', 'username',
+                    'email', 'first_name', 'last_name')
+    list_display_links = ('id', 'username')
+    search_fields = ('role', 'username', 'email')
+    list_filter = ('role', 'email', 'username')
 
 
+class SubscribeAdmin(BaseAdminSettings):
+    """Кастомизация админ панели для управления подписками."""
+    list_display = ('id', 'user', 'author')
+    list_display_links = ('id', 'user')
+    search_fields = ('user__username', 'author__username')
+    list_filter = ('user', 'author')
+
+
+# Регистрируем модели в админке
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Subscribe, SubscribeAdmin)
