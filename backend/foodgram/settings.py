efd8ef5f79be15972bde13b3efd2e42f3f994ae4
NikitaPoruchikov <neoboom34@gmail.com>
2024-10-27 17:25:53 +0700

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-CSRF_TRUSTED_ORIGINS = [os.getenv('DJANGO_DOMEN', 'default_value')]
+CSRF_TRUSTED_ORIGINS = [os.getenv('DJANGO_DOMEN', 'https://example.com')]
 
 
 # Application definition
